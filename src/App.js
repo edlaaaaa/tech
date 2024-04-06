8617e32f736543fdfb4762868876bee822c15791
rahul <anamshkhan5@gmail.com>
2024-04-06 05:52:04 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {Switch, Route, Redirect} from 'react-router-dom'
+import Home from './components/Home'
+import CourseItemDetails from './components/CourseItemDetails'
+import './App.css'
+import NotFound from './components/NotFound'
+
+const App = () => (
+  <div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/courses/:id" component={CourseItemDetails} />
+      <Route exact path="/not-found" component={NotFound} />
+    </Switch>
+  </div>
+)
+
+export default App
